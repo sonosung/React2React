8b4c971db8e8771ad376c8e1828bc63badf8089d
msh07 <msh071@naver.com>
2024-10-22 23:28:56 +0900

    2024.10.22 리액트 프로젝트 생성

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          My Instagram
+          My Instagram/sonosung
         </a>
       </header>
     </div>
