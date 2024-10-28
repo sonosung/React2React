1b8f21606b35803bfd65a1d9a1a7a8fcd4f562ba
msh07 <msh071@naver.com>
2024-10-28 23:44:23 +0900

    2024.10.28 JSX 문법 배우기 시작!

--- src/App.js
+++ src/App.js
@@ -2,22 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let post = 'hello!'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          wellcome <code>to </code>my wrold!
-        </p>
-        <a
-          className="App-link"
-          href="https://www.instagram.com/sono_sung/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          My Instagram/sonosung
-        </a>
-      </header>
+      <div className="top-bar">
+       <h4 style={{color : 'white'}}>{ post }</h4>
+      </div>
+      
     </div>
   );
 }
